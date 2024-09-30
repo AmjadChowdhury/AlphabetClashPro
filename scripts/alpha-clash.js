eb043153a50b9c574a1bec028fcb46831119969a
AmjadChowdhury <amjadchowdhury247@gmail.com>
2024-09-30 18:48:36 +0100

    function create for key press...

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,7 +1,24 @@
+function handleKeyPress(event){
+    const keyPressed = event.key
+
+    const currentAlphabet = document.getElementById('currentAlphabet')
+    const alphabet = currentAlphabet.innerText
+    const expectedAlphabet = alphabet.toLowerCase()
+
+    if(keyPressed == expectedAlphabet){
+        console.log('Dui Dui e mile gese...')
+    }
+    else{
+        console.log('Kichui mile nai')
+    }
+}
+
+document.addEventListener('keyup',handleKeyPress)
+
+
 function continueGame(){
     // step 1: generate a random alphabet..
     const alphabet = getRandomAlphabet()
-    console.log(alphabet)
 
     // step 2: Set randomly generated alphabet to the screen(show it)
     const currentAlphabet = document.getElementById('currentAlphabet')
